35f002788f0e7e75b962c8dc49a2e6e40e9850a8
tmclean85 <trevor.a.mclean@gmail.com>
2017-05-19 14:32:03 -0700

    First commit

---
+++ themes/inhabitent-theme/inc/extras.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Custom functions that act independently of the theme templates.
+ *
+ * @package RED_Starter_Theme
+ */
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function red_starter_body_classes( $classes ) {
+	// Adds a class of group-blog to blogs with more than 1 published author.
+	if ( is_multi_author() ) {
+		$classes[] = 'group-blog';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'red_starter_body_classes' );
+
+// Remove "Editor" links from sub-menus
+// function inhabitent_remove_submenus() {
+//     remove_submenu_page( 'themes.php', 'theme-editor.php' );
+//     remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
+// }
+// add_action( 'admin_menu', 'inhabitent_remove_submenus', 110 );
+
+
+
