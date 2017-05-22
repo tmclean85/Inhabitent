b8773f3a44136e4f184fb64e3dd04a918a967460
tmclean85 <trevor.a.mclean@gmail.com>
2017-05-22 15:36:16 -0700

    Styled homepage

--- themes/inhabitent-theme/header.php
+++ themes/inhabitent-theme/header.php
@@ -18,10 +18,12 @@
 
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
+			<div class="hero-main">
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
+					<a href="#"><img class="nav-logo" src="/wordpress/wp-content/themes/inhabitent-theme/images/inhabitent-logo-tent-white.svg" alt="small-inhabitent-logo"></a>
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
@@ -30,9 +32,13 @@
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
-
+				</header><!-- #masthead -->
+				<div class="main-logo-wrapper">
+				<img src="/wordpress/wp-content/themes/inhabitent-theme/images/inhabitent-logo-full.svg">
+				</div> <!-- .main-logo=-wrapper -->
+				</div> <!-- .hero-main -->
 <!--Code for search here-->
 
-			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
+
