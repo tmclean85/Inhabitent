613df7c2b75ff1f6823a6cb5921e12b5e7dbb7ce
tmclean85 <trevor.a.mclean@gmail.com>
2017-05-28 21:51:02 -0700

    Correct indenting in front-page.php

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -1,25 +1,22 @@
 <?php get_header(); ?>
 <?php hero_banner(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
 		<?php if ( have_posts() ) : ?>
+		<?php if ( is_home() && ! is_front_page() ) : ?>
+		<header>
+			<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+		</header>
+	  <?php endif; ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+		  <?php get_template_part( 'template-parts/content' ); ?>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
+		<?php endwhile; ?>
 
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
+		<?php the_posts_navigation(); ?>
 
 		<?php else : ?>
 
@@ -28,37 +25,33 @@
 		<?php endif; ?>
 	
 <!-- store categories -->
-  <div class="journal-wrapper-main">
-    <h1>SHOP STUFF</h1>
-	</div>
-				<?php 
-					$categories = array(
-						'taxonomy' 		=> 'product-type', 
-						'hide_empty' 	=> true,
-						);
-					$terms = get_terms( $categories );
-				?>
-	<section class="product-types">
-
-				<?php foreach ( $terms as $term ) :  ?>
-  
-    <div class="product-type-box">
-      <ul class="shop-categories">				
-				<li class="shop-category-box"> 						
-					<img src="<?php echo get_template_directory_uri(). "/images/product-type-icons/" .$term->slug. ".svg" ?> " alt="product-type">
-					<p><?php echo $term->description?></p>
-					<a href="<?php echo get_term_link( $term ); ?>"> 						
-						<button class="button-shop">
-							<?php echo $term->name . ' Stuff'?>
-						</button>
-					</a>
-				</li>
-     </div><!--.product-type-box-->			
-				<?php endforeach; ?>
-			</ul>
-		</section> <!--.product-types -->
-
-
+    <div class="journal-wrapper-main">
+      <h1>SHOP STUFF</h1>
+	  </div>
+		<?php 
+		  $categories = array(
+			  'taxonomy' 		=> 'product-type', 
+				'hide_empty' 	=> true,
+				);
+			$terms = get_terms( $categories );
+		?>
+	  <section class="product-types">
+		  <?php foreach ( $terms as $term ) :  ?>
+      <div class="product-type-box">
+        <ul class="shop-categories">				
+				  <li class="shop-category-box"> 						
+					  <img src="<?php echo get_template_directory_uri(). "/images/product-type-icons/" .$term->slug. ".svg" ?> " alt="product-type">
+					  <p><?php echo $term->description?></p>
+					  <a href="<?php echo get_term_link( $term ); ?>"> 						
+						  <button class="button-shop">
+							  <?php echo $term->name . ' Stuff'?>
+						  </button>
+					  </a>
+				  </li>
+      </div><!--.product-type-box-->			
+		  <?php endforeach; ?>
+		 </ul>
+	  </section> <!--.product-types -->
 <!-- get posts -->
 <?php
 		$postArgs = array(
@@ -84,7 +77,7 @@ $posts = new WP_Query( $postArgs ); ?>
 	   <ul class="home-posts">
 		   <li><?php the_post_thumbnail( 'medium' ); ?></a></li>
 			 <div class="home-post-footer">
-			   <li class="date-item"><?php the_time('F jS, Y'); ?> / <?php comments_number( '0 Comments', '1 0 Comment', '% 0 Comments' ); ?></li>
+			   <li class="date-item"><?php the_time('F jS, Y'); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?></li>
 		     <li class="journal-post-title"><a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a></li>
 		     <li><button><a href="<?php the_permalink(); ?>">READ ENTRY</a></button></li>
 			 </div>	 
@@ -128,8 +121,7 @@ $posts = new WP_Query( $postArgs ); ?>
 		</div>			
 	</div>
 	</div>	
-			<button class="adventures-button">MORE ADVENTURES</button>	
-
+	<button class="adventures-button">MORE ADVENTURES</button>	
 </div>
 	
 
