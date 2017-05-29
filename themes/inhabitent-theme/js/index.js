7c6ef4c804a7a150ac6bab7e942036db5fd9fcd5
tmclean85 <trevor.a.mclean@gmail.com>
2017-05-29 08:57:30 -0700

    jQuery troubleshooting attempt

--- themes/inhabitent-theme/js/index.js
+++ themes/inhabitent-theme/js/index.js
@@ -1,13 +1,15 @@
-$(document).ready(function (){
+$(document).ready(function(){
   var $searchIcon = $('#search-icon');
   var $searchInput = $('.search-form');
+
+  console.log('mic check');
 
   $searchIcon.click(function(event){
     event.preventDefault();  
     $searchInput.toggleClass('search-form-long');
   });
 
-  $searchInput.focusout(function(event){
+  $searchInput.focusout(function(){
      $searchInput.toggleClass('search-form-long');
   });
 
